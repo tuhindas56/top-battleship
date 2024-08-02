31bfa28d8d3aea773342addd47fea267ef22b6b6
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-02 09:30:57 +0530

    UI: Create the UI

---
+++ src/ts/dom/ui.ts
@@ -0,0 +1,95 @@
+const header = document.querySelector("header") as HTMLElement
+const main = document.querySelector("main") as HTMLElement
+
+const parentContainer = document.createElement("div") as HTMLDivElement
+parentContainer.className = "container"
+
+const setupHeading = () => {
+  const heading = document.createElement("p") as HTMLParagraphElement
+
+  heading.className = "heading"
+  heading.innerHTML =
+    'Battleship<span> built by <a href="https://github.com/tuhindas56/" target="_blank" rel="noopener noreferrer">Tuhin Das</a></span>'
+
+  header.appendChild(heading)
+}
+
+const setupInfoText = () => {
+  const text = document.createElement("p") as HTMLParagraphElement
+
+  text.className = "text"
+  text.textContent = "Place your ships"
+
+  main.appendChild(text)
+}
+
+const setupLabels = () => {
+  const rows = document.createElement("div") as HTMLDivElement
+  const columns = document.createElement("div") as HTMLDivElement
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+  rows.className = "rows"
+  columns.className = "columns"
+
+  alphabet.forEach((letter) => {
+    const span = document.createElement("span") as HTMLSpanElement
+    span.textContent = letter
+    rows.appendChild(span)
+  })
+
+  for (let i = 0; i < 10; i += 1) {
+    const span = document.createElement("span") as HTMLSpanElement
+    span.textContent = `${i}`
+    columns.appendChild(span)
+  }
+
+  parentContainer.appendChild(rows)
+  parentContainer.appendChild(columns)
+  main.appendChild(parentContainer)
+}
+
+const setupGridInDOM = () => {
+  const gridContainer = document.createElement("div") as HTMLDivElement
+
+  gridContainer.className = "grid-container"
+
+  for (let i = 0; i < 100; i += 1) {
+    const button = document.createElement("button")
+    button.className = "square"
+    gridContainer.appendChild(button)
+  }
+
+  parentContainer.appendChild(gridContainer)
+}
+
+const setupShipPickingArea = () => {
+  const shipContainer = document.createElement("div") as HTMLDivElement
+  const shipNames = [
+    "carrier",
+    "battleship",
+    "destroyer",
+    "submarine",
+    "patrol-boat",
+  ]
+
+  shipContainer.className = "ships"
+
+  shipNames.forEach((name) => {
+    const ship = document.createElement("div") as HTMLDivElement
+    ship.className = `ship ${name}`
+    ship.draggable = true
+    shipContainer.appendChild(ship)
+  })
+
+  main.appendChild(shipContainer)
+}
+
+const setupUI = () => {
+  setupHeading()
+  setupInfoText()
+  setupLabels()
+  setupGridInDOM()
+  setupShipPickingArea()
+}
+
+export default setupUI
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,4 @@
-import '../style.css'
+import "../style.css"
+import setupUI from "./dom/ui"
+
+setupUI()
