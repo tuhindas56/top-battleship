f2f8ef395c87e1951615a94feaf7752653f19a18
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-03 22:29:49 +0530

    refactor: Replace generated DOM content with static HTML
    - Simplified code by reducing reliance on JavaScript for content
    rendering
    - Added logic to attach row and column indexes to generated grid cells

--- src/ts/dom/ui.ts
+++ src/ts/dom/ui.ts
@@ -1,28 +1,5 @@
-const header = document.querySelector("header") as HTMLElement
 const main = document.querySelector("main") as HTMLElement
-const footer = document.querySelector("footer") as HTMLElement
-
-const parentContainer = document.createElement("div") as HTMLDivElement
-parentContainer.className = "container"
-
-const setupHeading = () => {
-  const heading = document.createElement("p") as HTMLParagraphElement
-
-  heading.className = "heading"
-  heading.innerHTML =
-    'Battleship<span> built by <a href="https://github.com/tuhindas56/" target="_blank" rel="noopener noreferrer">Tuhin Das</a></span>'
-
-  header.appendChild(heading)
-}
-
-const setupInfoText = () => {
-  const text = document.createElement("p") as HTMLParagraphElement
-
-  text.className = "text"
-  text.textContent = "Place your ships"
-
-  main.appendChild(text)
-}
+const parentContainer = document.querySelector(".container") as HTMLDivElement
 
 const setupLabels = () => {
   const rows = document.createElement("div") as HTMLDivElement
@@ -32,67 +9,43 @@ const setupLabels = () => {
   rows.className = "rows"
   columns.className = "columns"
 
-  alphabet.forEach((letter) => {
-    const span = document.createElement("span") as HTMLSpanElement
-    span.textContent = letter
-    rows.appendChild(span)
-  })
+  alphabet.forEach((letter, index) => {
+    const column = document.createElement("span") as HTMLSpanElement
+    const row = document.createElement("span") as HTMLSpanElement
 
-  for (let i = 0; i < 10; i += 1) {
-    const span = document.createElement("span") as HTMLSpanElement
-    span.textContent = `${i}`
-    columns.appendChild(span)
-  }
+    column.textContent = `${index}`
+    row.textContent = letter
+
+    columns.appendChild(column)
+    rows.appendChild(row)
+  })
 
   parentContainer.appendChild(rows)
   parentContainer.appendChild(columns)
   main.appendChild(parentContainer)
 }
 
-const setupGridInDOM = () => {
+const createGrid = () => {
   const gridContainer = document.createElement("div") as HTMLDivElement
 
   gridContainer.className = "grid-container"
 
-  for (let i = 0; i < 100; i += 1) {
-    const button = document.createElement("button")
-    button.className = "square"
-    gridContainer.appendChild(button)
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      const button = document.createElement("button")
+      button.className = "square"
+      button.dataset.row = `${i}`
+      button.dataset.column = `${j}`
+      gridContainer.appendChild(button)
+    }
   }
 
   parentContainer.appendChild(gridContainer)
 }
 
-const setupShipPickingArea = () => {
-  const shipContainer = document.createElement("div") as HTMLDivElement
-  const shipNames = ["carrier", "battleship", "destroyer", "submarine", "patrol-boat"]
-
-  shipContainer.className = "ships"
-
-  shipNames.forEach((name) => {
-    const ship = document.createElement("div") as HTMLDivElement
-    ship.className = `ship`
-    ship.id = name
-    ship.draggable = true
-    shipContainer.appendChild(ship)
-  })
-
-  main.appendChild(shipContainer)
-}
-
-const setupFooter = () => {
-  const footerText = document.createElement("p") as HTMLParagraphElement
-  footerText.textContent = "Battleship icon by Icons8"
-  footer.appendChild(footerText)
-}
-
-const setupUI = () => {
-  setupHeading()
-  setupInfoText()
+const setupGrid = () => {
   setupLabels()
-  setupGridInDOM()
-  setupShipPickingArea()
-  setupFooter()
+  createGrid()
 }
 
-export default setupUI
+export default setupGrid
