cc04e6ef8c450b8f13c7b1b72aa01332f92a3763
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-04 19:41:39 +0530

    refactor: Remove redundant checks, add removeShip method
    - Remove redundant 'if' checks
    - Implement a removeShip method for when player moves his ship to
    a new location
    - Switch shipsPlaced array to a Set
    - Verify if a ship is in the path before starting the addition process

--- src/ts/gameBoard/gameBoard.ts
+++ src/ts/gameBoard/gameBoard.ts
@@ -3,34 +3,26 @@ import Ship from "../ship/ship"
 type Coordinate = { row: number; column: number; vertical?: boolean }
 
 export default class Gameboard {
-  gameBoard: Ship[][] = []
+  gameBoard: Ship[][] | null[][] = []
   attackedCoordinates: Set<string> = new Set()
   missedAttacks: Coordinate[] = []
   #numberOfShips: number = 0
-  #shipsPlaced: Ship[] = []
+  #shipsPlaced: Set<Ship> = new Set()
 
   constructor() {
-    for (let i = 0; i < 10; i += 1)
-      this.gameBoard.push(new Array(10).fill(null))
+    for (let i = 0; i < 10; i += 1) this.gameBoard.push(new Array(10).fill(null))
   }
 
   #hasShip(ship: Ship) {
-    return this.#shipsPlaced.some(
-      (existingShip) => existingShip.type === ship.type,
-    )
+    return this.#shipsPlaced.has(ship)
   }
 
   #checkIfAllShipsAreSunk() {
-    return this.#shipsPlaced.every((ship) => ship.isSunk())
+    return Array.from(this.#shipsPlaced).every((ship) => ship.isSunk())
   }
 
   placeShip(ship: Ship, { row, column, vertical = false }: Coordinate) {
-    if (this.#hasShip(ship)) {
-      throw new TypeError("A ship of the same type was already placed") // Check if board already has a ship of the same type
-    }
-    if (this.#numberOfShips === 5) {
-      throw new RangeError("Cannot place more than five ships") //  Check if board already has five ships
-    }
+    if (this.#hasShip(ship)) this.#removeShip(ship, vertical)
 
     const range = vertical ? row + ship.size : column + ship.size
     if (range > 10) {
@@ -38,23 +30,44 @@ export default class Gameboard {
     }
 
     for (let i = 0; i < ship.size; i += 1) {
+      if (
+        (vertical && this.gameBoard[row + i][column]) ||
+        (!vertical && this.gameBoard[row][column + i])
+      ) {
+        throw new Error("Ship placement range is not empty") // If a cell in the range of cells is occupied, throw an error
+      }
+    }
+
+    for (let i = 0; i < ship.size; i += 1) {
       if (vertical) {
-        if (this.gameBoard[row + i][column]) {
-          throw new Error("Ship placement range is not empty") // If a cell in the range of cells is occupied, throw an error
-        }
-        this.gameBoard[row + i][column] = ship // Else, place the ship
+        this.gameBoard[row + i][column] = ship // Place the ship
       } else {
-        if (this.gameBoard[row][column + i]) {
-          throw new Error("Ship placement range is not empty")
-        }
         this.gameBoard[row][column + i] = ship
       }
     }
 
     this.#numberOfShips += 1 // Increment number of ships placed
-    this.#shipsPlaced.push(ship) // Track type of ship placed
+    this.#shipsPlaced.add(ship) // Track type of ship placed
 
     return "placed"
+  }
+
+  #removeShip(ship: Ship, vertical: boolean) {
+    let column = 0
+    let row = 0
+
+    for (row; row < this.gameBoard.length; row += 1) {
+      column = this.gameBoard[row].findIndex((placedShip) => placedShip === ship)
+      if (!(column < 0)) break
+    }
+
+    for (let i = 0; i < ship.size; i += 1) {
+      if (vertical) {
+        this.gameBoard[row + i][column] = null
+      } else {
+        this.gameBoard[row][column + i] = null
+      }
+    }
   }
 
   receiveAttack({ row, column }: Coordinate) {
