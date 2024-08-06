6499a7dae8b6a77227d837fc440a4afa01ec111b
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-06 21:29:19 +0530

    feat: Setup functions that subscribe to events
    - Setup functions that subscribe to events emitted by modules, and
    execute code accordingly

--- src/ts/dom/domController.ts
+++ src/ts/dom/domController.ts
@@ -1,4 +1,5 @@
-import Player from "../player/player"
+import gameController from "../gameController/gameController"
+import { setupGrid, showWinner, switchView } from "./ui"
 import {
   allowDrop,
   drag,
@@ -7,16 +8,13 @@ import {
   placeShips,
   removeShipHighlight,
   rotateShips,
+  updateTextDisplay,
 } from "./domControllerUtils"
-import { setupGrid } from "./ui"
 
 const computerContainer = document.querySelector(".container#computer") as HTMLDivElement
 const playerContainer = document.querySelector(".container#player") as HTMLDivElement
 
 let currentOrientation: "horizontal" | "vertical" = "horizontal"
-const computer = new Player()
-const player = new Player()
-let currentTurn = "player"
 
 const setupDragAndDrop = () => {
   const gridCells = document.querySelectorAll(".square") as NodeListOf<HTMLButtonElement>
@@ -33,9 +31,9 @@ const setupDragAndDrop = () => {
   gridCells.forEach((cell) => {
     cell.addEventListener("dragover", (event) => {
       allowDrop(event)
-      cell.classList.add("hovered")
+      cell.classList.add("dragging")
     })
-    cell.addEventListener("dragleave", () => cell.classList.remove("hovered"))
+    cell.addEventListener("dragleave", () => cell.classList.remove("dragging"))
     cell.addEventListener("drop", drop)
   })
 }
@@ -47,13 +45,121 @@ const setupOrientationSwitching = () => {
   })
 }
 
-const setupShipPlacement = (player1: Player, player2: Player) => {
-  playerContainer.addEventListener("shipPlaced", ((event: CustomEvent) => {
-    placeShips(event, currentTurn === "player" ? player1 : player2, currentOrientation)
+const setupShipPlacement = (container: HTMLDivElement) => {
+  const shipContainer = document.querySelector(".ships") as HTMLDivElement
+  const ships = shipContainer.children
+  const switchOrientation = document.querySelector(".switch-orientation") as HTMLButtonElement
+  const acceptBtn = document.querySelector(".button.accept") as HTMLButtonElement
+  const cellContainer = container.querySelector(".grid-container") as HTMLDivElement
+
+  acceptBtn.disabled = true
+
+  cellContainer.addEventListener("shipPlaced", ((event: CustomEvent) => {
+    placeShips(event, gameController.player, currentOrientation)
+    acceptBtn.disabled = ships.length !== 0
+    switchOrientation.disabled = ships.length === 0
+  }) as EventListener)
+}
+
+const attachListenersToCells = (container: HTMLDivElement) => {
+  const cells = container.querySelectorAll(
+    ".grid-container .square",
+  ) as NodeListOf<HTMLButtonElement>
+
+  cells.forEach((cell) => {
+    cell.addEventListener(
+      "click",
+      (event: MouseEvent) => {
+        const target = event.target as HTMLButtonElement
+
+        target.classList.remove("hovered")
+        target.style.cursor = "auto"
+
+        const cellAttackedEvent = new CustomEvent("cellAttacked", {
+          bubbles: true,
+          detail: { row: cell.dataset.row, column: cell.dataset.column },
+        })
+
+        document.dispatchEvent(cellAttackedEvent)
+      },
+      { once: true },
+    )
+  })
+}
+
+const setupViewSwitch = () => {
+  const acceptBtn = document.querySelector(".button.accept") as HTMLButtonElement
+
+  acceptBtn.addEventListener("click", () => {
+    switchView()
+    setupGrid(computerContainer, true)
+    attachListenersToCells(computerContainer)
+    gameController.computer.autoPlaceShips()
+    updateTextDisplay("Your turn")
+  })
+}
+
+const setupAttackReception = () => {
+  document.addEventListener("cellAttacked", ((event: CustomEvent) => {
+    gameController.receiveAttack(event.detail)
+  }) as EventListener)
+
+  document.addEventListener("hit", ((event: CustomEvent) => {
+    const {
+      allShipsSunk,
+      by,
+      coordinate: { row, column },
+      isSunk,
+      shipType,
+    } = event.detail
+
+    if (!allShipsSunk) {
+      if (isSunk) {
+        updateTextDisplay(
+          `${by === "player" ? "You have" : "Computer has"} destroyed a ${shipType}`,
+        )
+      } else {
+        updateTextDisplay(`${by === "player" ? "You" : "Computer"} landed a hit on a ${shipType}`)
+      }
+    } else {
+      showWinner(by)
+    }
+
+    const cell = document.querySelector(
+      `${by !== "player" ? "#player" : "#computer"} .square[data-row='${row}'][data-column='${column}']`,
+    ) as HTMLButtonElement
+    cell.classList.add("hit")
+  }) as EventListener)
+
+  document.addEventListener("miss", ((event: CustomEvent) => {
+    const {
+      by,
+      coordinate: { row, column },
+    } = event.detail
+
+    updateTextDisplay(`${by === "player" ? "You" : "Computer"} missed`)
+
+    const cell = document.querySelector(
+      `${by !== "player" ? "#player" : "#computer"} .square[data-row='${row}'][data-column='${column}']`,
+    ) as HTMLButtonElement
+    cell.classList.add("miss")
+  }) as EventListener)
+
+  document.addEventListener("changeturn", ((event: CustomEvent) => {
+    if (event.detail === "computer") {
+      computerContainer.classList.add("disableClick", "lowOpacity")
+      playerContainer.classList.remove("disableClick", "lowOpacity")
+      setTimeout(() => gameController.computerAttack(), 2000)
+    } else {
+      computerContainer.classList.remove("disableClick", "lowOpacity")
+      playerContainer.classList.add("disableClick", "lowOpacity")
+    }
   }) as EventListener)
 }
 
 setupGrid(playerContainer)
 setupDragAndDrop()
 setupOrientationSwitching()
-setupShipPlacement(player, computer)
+setupShipPlacement(playerContainer)
+setupViewSwitch()
+setupAttackReception()
