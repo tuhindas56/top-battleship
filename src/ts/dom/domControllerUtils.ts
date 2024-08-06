04909bcea828a7213595464cf43361bb757f316d
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-06 21:26:25 +0530

    style: Move drag and drop functions, add text display update logic
    - Move drag and drop functions to the end of the module
    - Create updateTextDisplay function to update text during gameplay

--- src/ts/dom/domControllerUtils.ts
+++ src/ts/dom/domControllerUtils.ts
@@ -1,36 +1,5 @@
 import Player from "../player/player"
 
-export const drag = (event: DragEvent) => {
-  const target = event.target as HTMLDivElement
-  event.dataTransfer?.setData("text", target.id)
-}
-
-export const allowDrop = (event: MouseEvent) => event.preventDefault()
-
-const checkDroppingConditions = (target: HTMLElement) => {
-  const ships = document.querySelectorAll(".ship") as NodeListOf<HTMLElement>
-  return ![...ships].some((item) => item === target)
-}
-
-export const drop = (event: DragEvent) => {
-  event.preventDefault()
-
-  const data = event.dataTransfer?.getData("text")
-  const target = event.target as HTMLElement
-
-  if (checkDroppingConditions(target)) {
-    const ship = document.querySelector(`#${data}`) as HTMLDivElement
-    ship.classList.add("placed")
-    target.appendChild(ship)
-
-    const shipPlacementEvent = new CustomEvent("shipPlaced", {
-      bubbles: true,
-      detail: target,
-    })
-    ship.dispatchEvent(shipPlacementEvent)
-  }
-}
-
 export const highlightShip = (event: MouseEvent) => {
   const target = event.target as HTMLButtonElement
   target.classList.add("ship-highlight")
@@ -62,18 +31,52 @@ export const placeShips = (
   const cell = event.detail
   const shipType: Ships = target.id as Ships
   const [row, column] = [+cell.dataset.row, +cell.dataset.column]
-
-  try {
-    player.gameBoard.placeShip(player.ships[shipType], {
-      row,
-      column,
-      vertical: currentOrientation !== "horizontal",
-    })
-  } catch (error) {
+  const placementResult = player.gameBoard.placeShip(player.ships[shipType], {
+    row,
+    column,
+    vertical: currentOrientation !== "horizontal",
+  })
+  if (placementResult !== "placed") {
     const temp = target
     target.remove()
     cell.classList.remove("hovered")
     temp.className = currentOrientation !== "horizontal" ? "ship vertical" : "ship"
     shipsContainer.appendChild(temp)
   }
+}
+
+export const drag = (event: DragEvent) => {
+  const target = event.target as HTMLDivElement
+  event.dataTransfer?.setData("text", target.id)
+}
+
+export const allowDrop = (event: MouseEvent) => event.preventDefault()
+
+const checkDroppingConditions = (target: HTMLElement) => {
+  const ships = document.querySelectorAll(".ship") as NodeListOf<HTMLElement>
+  return ![...ships].some((item) => item === target)
+}
+
+export const drop = (event: DragEvent) => {
+  event.preventDefault()
+
+  const data = event.dataTransfer?.getData("text")
+  const target = event.target as HTMLElement
+  const shipPlacementEvent = new CustomEvent("shipPlaced", {
+    bubbles: true,
+    detail: target,
+  })
+
+  if (checkDroppingConditions(target)) {
+    const ship = document.querySelector(`#${data}`) as HTMLDivElement
+    ship.classList.add("placed")
+    target.appendChild(ship)
+
+    ship.dispatchEvent(shipPlacementEvent)
+  }
+}
+
+export const updateTextDisplay = (status: string) => {
+  const text = document.querySelector(".text") as HTMLParagraphElement
+  text.textContent = status
 }
